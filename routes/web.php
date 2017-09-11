1e0c50767e4517c869cfe4c8f40e0e721d67978c
AlekseiV <aleksei_voronin@hotmail.com>
2017-09-11 14:52:41 +0000

    Gagnasafnsfræði tv6

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ Route::get('/threads/create', function() {
 	return view('threads.create');
 });
 
-Route::get('/greeting/{id}/{count}', function($id, $count) {
-	return view('messages.greeting', compact("id" , "count"));
-})->where(['id' => '[0-9]+' , 'count' => '[0-9]+']);
+Route::get('/greeting/{id}', function($id) {
+	$greetings = ["Velkominn á síðuna", "Bonjour", "Siema", "Здра́вствуйте", "Welcome on the site"];
+	return view('messages.greeting', ["greeting" => $greetings[$id]]);
+})->where(['id' => '[0-4]']);
+
+// Route::get('/greeting/{id}/{count}', function($id, $count) {
+// 	return view('messages.greeting', compact("id" , "count"));
+// })->where(['id' => '[0-9]+' , 'count' => '[0-9]+']);
