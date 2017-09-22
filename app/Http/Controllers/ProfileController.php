ce1e0f0eb35f3c1275a46a38a5b8231287a46935
AlekseiV <aleksei_voronin@hotmail.com>
2017-09-22 10:02:50 +0000

    Heimaverk 2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,15 +9,13 @@ class ProfileController extends Controller
 {
   public function index(){
     $names = DB::select("select name from users");
+    $email = DB::select("select email from users");
     $joined = DB::select("select created_at from users");
     $profilethreads = DB::select("select * from threads");
     $threadsinfo = [
-        ["nafn" => "Aleksei Voronin"],
-        ["published" => "Laravel Controller", "anchor" => ""],
-        ["published" => "Laravel 5.5", "anchor" => "" ],
-        ["body" => "Hvernig býr maður til controller í Laravel?"],
-        ["body" => "Hver er munurinn á Laravel útgáfu 5.4 og 5.5?"]
+        ["published" => "Laravel Controller", "body" => "Hvenig býr maður til controller í Laravel?", "anchor" => "https://laravel.com/docs/5.4/controllers"],
+        ["published" => "Laravel 5.5", "body" => "Hver er munurinn á Laravel útgáfu 5.4 og 5.5?", "anchor" => "" ],
       ];
-    return view('profile.profile', compact("profilethreads", "names", "joined", "threadsinfo"));
+    return view('profile.profile', compact("profilethreads", "names", "joined", "threadsinfo", "email"));
   }
 }
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,20 +3,26 @@
 @section('content')
 <div class="container">
   <h1>{{ $names[0]->name }}<small> meðlimur siðan {{ $joined[0]->created_at }}</small></h1>
-    <div class="row">
-      INFORMATION
-        <div class="col-md-8 col-md-offset-2">
+      <div class="row">
+        <div class="col-md-9">
             @foreach ($threadsinfo as $info)
               <div class="panel panel-default">
                 <!-- Default panel contents -->
-                <div class="panel-heading"><b> {{ $info[0]->nafn }} </b></div>
+                <div class="panel-heading"><b> {{ $names[0]->name }} published <a href="{{ $info["anchor"] }}">{{ $info["published"] }}</a></b></div>
                 <ul class="list-group">
                   <div class="list-group-item">
-                    {{ $info[1]->published }}
+                    {{ $info["body"] }}
                   </div>
                 </ul>
               </div>
               @endforeach
+        </div>
+        <div class="col-md-3">
+          <div class="panel">
+            <div class="panel-heading">Nafn : {{$names[0]->name}}</div>
+                <div class="panel-heading">Netfang : {{$email[0]->email}}</div>
+                <div class="panel-heading">Fjöldi þráða : 14</div>
+            </div>
         </div>
     </div>
 </div>
